39d3135af98f325efbe38ec0912818ca8b268140
Alain <snowgoon88@gmail.com>
2013-01-10 13:40:22 +0100

    bsearch ok, pos depart fixe

--- pyrob.py
+++ pyrob.py
@@ -5,16 +5,15 @@ import gtk
 from board import *
 
 # ******************************************************************************
-# ************************************************************************* TODO
+# ************************************************************************ PyRob
 # ******************************************************************************
 class PyRob(object):
     """Application pour Ricochet Robot
     """
     
-    # --------------------------------------------------------------------------
-    # --------------------------------------------------------------------- todo
+    # --------------------------------------------------------------------- init
     def __init__(self, ):
-        """
+        """Crée une fenetre avec Board et Boutons
         """
         # Board
         self._board = create_board16()
@@ -22,6 +21,7 @@ class PyRob(object):
         self._board._tree.append( self._btree )
         #
         window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.set_title( "Ricochet Robots" )
         window.resize(750,600)
         window.connect("delete-event", gtk.main_quit)
         #
@@ -31,12 +31,12 @@ class PyRob(object):
         #
         window.present()
         gtk.main()
-    # --------------------------------------------------------------------------
-    # --------------------------------------------------------------------- todo
+
+    # ---------------------------------------------------------------- build_gui
     def build_gui(self, ):
         """
         HBox { Board, VBox}
-        VBox { reset_btn, step_btn }
+        VBox { {btn for bsearch }
         """
         main_hbox = gtk.HBox(homogeneous=False, spacing=0)
         main_hbox.pack_start(self._board, expand=True, fill=True, padding=0)
@@ -49,13 +49,24 @@ class PyRob(object):
         btn_vbox.pack_start(label, expand=False, fill=False, padding=0)
         label.show()
         reset_btn = gtk.Button("Reset")
-        reset_btn.connect( "clicked", self.bsearch_reset_cbk, "Reset")
+        reset_btn.connect( "clicked", self.bsearch_reset_cbk, "Reset_BT")
         btn_vbox.pack_start(reset_btn, expand=False, fill=False, padding=0)
         reset_btn.show()
-        step_btn = gtk.Button("Step")
-        step_btn.connect( "clicked", self.bsearch_step_cbk, "Step")
-        btn_vbox.pack_start(step_btn, expand=False, fill=False, padding=0)
-        step_btn.show()
+        self._bsearch_step_btn = gtk.Button("Step")
+        self._bsearch_step_btn.connect( "clicked", self.bsearch_step_cbk, "Step_BT")
+        self._bsearch_step_btn.set_sensitive( False )
+        btn_vbox.pack_start(self._bsearch_step_btn, expand=False, fill=False, padding=0)
+        self._bsearch_step_btn.show()
+        value_btn = gtk.CheckButton("valeurs")
+        value_btn.set_active( self._btree._draw_value )
+        value_btn.connect( "toggled", self.bsearch_valeur_cbk, "Valeur_BT")
+        btn_vbox.pack_start(value_btn, expand=False, fill=False, padding=0)
+        value_btn.show()
+        arc_btn = gtk.CheckButton("arcs")
+        arc_btn.set_active( self._btree._draw_arc )
+        arc_btn.connect( "toggled", self.bsearch_arc_cbk, "Arc_BT")
+        btn_vbox.pack_start(arc_btn, expand=False, fill=False, padding=0)
+        arc_btn.show()
         
         # 
         separator = gtk.HSeparator()
@@ -63,8 +74,16 @@ class PyRob(object):
         separator.show()
         
         return main_hbox
-    # --------------------------------------------------------------------------
-    # --------------------------------------------------------------------- todo
+    # --------------------------------------------------------------- bbasic_cbk
+    def bbasic_cbk(self, widget, data=None):
+        """Callbak de base pour bouton
+        :Param
+        - `widget`: source
+        - `data`: extra data
+        """
+        print "btn  {0} pressed".format(data)
+        self._board.queue_draw()
+    # -------------------------------------------------------- bsearch_reset_cbk
     def bsearch_reset_cbk(self, widget, data=None):
         """Remet à zéro la recherche en profondeur.
         :Param
@@ -75,7 +94,10 @@ class PyRob(object):
         self._btree.clean()
         self._btree.build_step( (0,0) )
         print "Il reste {0} cell à explorer".format(len(self._btree._cell_to_expand))
+        if len(self._btree._cell_to_expand) > 0 :
+            self._bsearch_step_btn.set_sensitive( True )
         self._board.queue_draw()
+    # --------------------------------------------------------- bsearch_step_cbk
     def bsearch_step_cbk(self, widget, data=None):
         """Un step de recherche
         :Param
@@ -85,9 +107,34 @@ class PyRob(object):
         print "btn  {0} pressed".format(data)
         self._btree.expand_step()
         print "Il reste {0} cell à explorer".format(len(self._btree._cell_to_expand))
+        if len(self._btree._cell_to_expand) == 0 :
+            self._bsearch_step_btn.set_sensitive( False )
         self._board.queue_draw()
+    # ------------------------------------------------------- bsearch_valeur_cbk
+    def bsearch_valeur_cbk(self, widget, data=None):
+        """Affiche ou non les valeurs du Tree de recherche en largeur
+        :Param
+        - `widget`: source
+        - `data`: extra data
+        """
+        print "btn  {0} pressed".format(data)
+        self._btree._draw_value = widget.get_active()
+        self._board.queue_draw()
+    # ---------------------------------------------------------- bsearch_arc_cbk
+    def bsearch_arc_cbk(self, widget, data=None):
+        """Affiche ou non les arcs du Tree de recherche en largeur
+        :Param
+        - `widget`: source
+        - `data`: extra data
+        """
+        print "btn  {0} pressed".format(data)
+        self._btree._draw_arc = widget.get_active()
+        self._board.queue_draw()
+
+        
+
 # ******************************************************************************
-# ************************************************************************* TODO
+# ************************************************************************** END
 # ******************************************************************************
 
 if __name__ == '__main__':
